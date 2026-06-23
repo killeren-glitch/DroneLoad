0ac9499f2f0f86aaf78ac52107cff7a5700928ff
killeren-glitch <oliko1045@gmail.com>
2026-06-24 00:36:15 +0200

    Ajout de self et débuggage
    Refactor servo control methods and distance measurement logic.

--- sensors.py
+++ sensors.py
@@ -28,94 +28,9 @@ class VL53L5CXReader:
         self._sensor.set_ranging_mode(RANGING_MODE_CONTINUOUS)
 ####
         self._sensor.start_ranging()
-        def __patched_init(self, chip=None):
-            gpiozero.pins.lgpio.LGPIOFactory.__bases__[0].__init__(self)
-            chip = 0
-            self._handle = lgpio.gpiochip_open(chip)
-            self._chip = chip
-            self.pin_class = gpiozero.pins.lgpio.LGPIOPin
-
-        gpiozero.pins.lgpio.LGPIOFactory.__init__ = __patched_init
-        pin_servo1=17
-        pin_servo2=27
-
-        myCorrection=0.45
-        max_ang = 180.0
-        min_ang = 0.0
-        maxPW=(2.0+myCorrection)/1000
-        minPW=(1.0-myCorrection)/1000
         
-        servo1 = AngularServo(pin_servo1,min_angle=min_ang, max_angle=max_ang, min_pulse_width=minPW,max_pulse_width=maxPW)
-        servo2 = AngularServo(pin_servo2,min_angle=min_ang, max_angle=max_ang, min_pulse_width=minPW,max_pulse_width=maxPW)
         
-        t_servo1 = 0
-        t_servo2 = 0
-        etat_servo1="up"
-        etat_servo2="down"
-        
-    def servo_1_up(self):
-        global t_servo1 
-        global etat_servo1
-        global now_servo
-        now_servo = time.time()
     
-        if t_servo1 == 0:
-            t_servo1 = time.time()
-        if now_servo <= t_servo1 + 0.2:
-            if etat_servo1 == "down":
-                self.servo1.angle=10.0  
-        else : 
-            self.servo1.angle = None
-            etat_servo1 = "up"
-            t_servo1 = 0
-
-    def servo_1_down(self):
-        global t_servo1 
-        global etat_servo1
-        global now_servo1
-        now_servo1 = time.time()
-    
-        if t_servo1 == 0:
-            t_servo1 = time.time()
-        if now_servo1 <= t_servo1 + 0.2:
-            if etat_servo1 == "up":
-                self.servo1.angle=99.5  
-        else : 
-            self.servo1.angle = None
-            etat_servo1 = "down"
-            t_servo1 = 0
-    
-    def servo_2_up(self):
-        global t_servo2 
-        global etat_servo2
-        global now_servo2
-        now_servo2 = time.time()
-    
-        if t_servo2 == 0:
-            t_servo2 = time.time()
-        if now_servo2 <= t_servo2 + 0.2:
-            if etat_servo2 == "down":
-                self.servo2.angle=0.0 
-        else : 
-            self.servo2.angle = None
-            etat_servo2 = "up"
-            t_servo2 = 0
-    
-    def servo_2_down(self):
-        global t_servo2 
-        global etat_servo2
-        global now_servo2
-        now_servo2 = time.time()
-    
-        if t_servo2 == 0:
-            t_servo2 = time.time()
-        if now_servo2 <= t_servo2 + 0.2:
-            if etat_servo2 == "down":
-                self.servo2.angle=90.0 
-        else : 
-            self.servo2.angle = None
-            etat_servo2 = "up"
-            t_servo2 = 0
 
     def get_distance(self) -> float | None:
         """
@@ -141,26 +56,114 @@ class HardwareManager:
     def __init__(self):
         print("Initialisation du capteur VL53L5CX et des servos...")
         # TODO: init I2C pour VL53L5CX
-        # TODO: init PWM pour crochet et caméra
+        def __patched_init(self, chip=None):
+            gpiozero.pins.lgpio.LGPIOFactory.__bases__[0].__init__(self)
+            chip = 0
+            self._handle = lgpio.gpiochip_open(chip)
+            self._chip = chip
+            self.pin_class = gpiozero.pins.lgpio.LGPIOPin
+
+        gpiozero.pins.lgpio.LGPIOFactory.__init__ = __patched_init
+        pin_servo1=17
+        pin_servo2=27
+
+        myCorrection=0.45
+        max_ang = 180.0
+        min_ang = 0.0
+        maxPW=(2.0+myCorrection)/1000
+        minPW=(1.0-myCorrection)/1000
+        
+        self.servo1 = AngularServo(pin_servo1,min_angle=min_ang, max_angle=max_ang, min_pulse_width=minPW,max_pulse_width=maxPW)
+        self.servo2 = AngularServo(pin_servo2,min_angle=min_ang, max_angle=max_ang, min_pulse_width=minPW,max_pulse_width=maxPW)
+        
+        self.t_servo1 = 0
+        self.t_servo2 = 0
+        self.etat_servo1="up"
+        self.etat_servo2="down"
+
+
+    def servo_1_up(self):
+        global t_servo1 
+        global etat_servo1
+        global now_servo
+        self.now_servo = time.time()
+    
+        if self.t_servo1 == 0:
+            self.t_servo1 = time.time()
+        if self.now_servo <= self.t_servo1 + 0.2:
+            if self.etat_servo1 == "down":
+                self.servo1.angle=10.0  
+        else : 
+            self.servo1.angle = None
+            self.etat_servo1 = "up"
+            self.t_servo1 = 0
+
+    def servo_1_down(self):
+        global t_servo1 
+        global etat_servo1
+        global now_servo1
+        self.now_servo1 = time.time()
+    
+        if self.t_servo1 == 0:
+            self.t_servo1 = time.time()
+        if self.now_servo1 <= self.t_servo1 + 0.2:
+            if self.etat_servo1 == "up":
+                self.servo1.angle=99.5  
+        else : 
+            self.servo1.angle = None
+            self.etat_servo1 = "down"
+            self.t_servo1 = 0
+
+    def servo_2_up(self):
+        global t_servo2 
+        global etat_servo2
+        global now_servo2
+        self.now_servo2 = time.time()
+    
+        if self.t_servo2 == 0:
+            self.t_servo2 = time.time()
+        if self.now_servo2 <= self.t_servo2 + 0.2:
+            if self.etat_servo2 == "down":
+                self.servo2.angle=0.0 
+        else : 
+            self.servo2.angle = None
+            self.etat_servo2 = "up"
+            self.t_servo2 = 0
+    
+    def servo_2_down(self):
+        global t_servo2 
+        global etat_servo2
+        global now_servo2
+        self.now_servo2 = time.time()
+    
+        if self.t_servo2 == 0:
+            self.t_servo2 = time.time()
+        if self.now_servo2 <= self.t_servo2 + 0.2:
+            if self.etat_servo2 == "down":
+                self.servo2.angle=90.0 
+        else : 
+            self.servo2.angle = None
+            self.etat_servo2 = "up"
+            self.t_servo2 = 0
 
     def get_forward_distance(self):
         """ Retourne la distance vers l'avant en mètres """
         # TODO: Lire le capteur Pimoroni
         return 2.5  # Mock: retourne 2.5 mètres par défaut
 
-   def set_camera_pitch(self, cam_state):
+    def set_camera_pitch(self, cam_state):
         """ cam_state : -1 : caméra position haute
                         -0 : caméra position basse
         """
         #print(f"Caméra pivotée à {cam_state}°")
-        if cam_state == "1":
-            servo_1_down(self)
+        if cam_state == 1:
+            self.servo_1_down()
         else:
-            servo_1_up(self)
+            self.servo_1_up()
     
     def set_hook(self, open_state):
         #print(f"Crochet ouvert : {open_state}")
-        if open_state == "1":
+        if open_state == 1:
             self.servo_2_up()
-        if open_state == "0":
+        if open_state == 0:
             self.servo_2_down()
